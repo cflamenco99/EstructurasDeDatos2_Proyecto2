acae6fb64b33887ffa08a61d64c5e95ba0df68d6
Carlos Flamenco <cflamenco99@gmail.com>
2022-05-31 18:47:10 -0600

    Clase funciones

--- Proyecto2/Proyecto2/Program.cs
+++ Proyecto2/Proyecto2/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Proyecto 2");
+Console.WriteLine("Carlos Flamenco - 62011019");
