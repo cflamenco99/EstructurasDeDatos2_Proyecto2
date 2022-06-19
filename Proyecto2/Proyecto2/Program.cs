240ff3767067447d7c41e93f9d55bac03fe36b2d
Carlos Flamenco <cflamenco99@gmail.com>
2022-06-19 12:56:10 -0600

    video demostracion

--- Proyecto2/Proyecto2/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Proyecto 2");
-Console.WriteLine("Carlos Flamenco - 62011019");
-Console.WriteLine("Raul Trochez - 61841297");
-
